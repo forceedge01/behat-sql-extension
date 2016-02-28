d705c50a49b883f694075f27bd0d3fb15dfc421f
abdul <abdul@easyfundraising.org.uk>
2016-02-28 22:15:29 +0000

    Behat does not support step definitions on interfacess

--- src/Context/Interfaces/SQLContextInterface.php
+++ src/Context/Interfaces/SQLContextInterface.php
@@ -4,35 +4,15 @@ namespace Genesis\SQLExtension\Context\Interfaces;
 
 interface SQLContextInterface
 {
-    /**
-     * @Given /^I have an? "([^"]*)" where "([^"]*)"$/
-     * @Given /^I have an? "([^"]*)" with "([^"]*)"$/
-     */
     public function iHaveAWhere($entity, $columns);
 
-    /**
-     * @Given /^I don't have an? "([^"]*)" where "([^"]*)"$/
-     * @Given /^I don't have an? "([^"]*)" with "([^"]*)"$/
-     */
     public function iDontHaveAWhere($entity, $columns);
 
-    /**
-     * @Given /^I have an existing "([^"]*)" with "([^"]*)" where "([^"]*)"$/
-     */
     public function iHaveAnExistingWithWhere($entity, $with, $columns);
 
-    /**
-     * @Then /^I should have a "([^"]*)" with "([^"]*)"$/
-     */
     public function iShouldHaveAWith($entity, $with);
 
-    /**
-     * @Given /^I save the id as "([^"]*)"$/
-     */
     public function iSaveTheIdAs($key);
 
-    /**
-     * @Given /^I am in debug mode$/
-     */
     public function iAmInDebugMode();
 }
