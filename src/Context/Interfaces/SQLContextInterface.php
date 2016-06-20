acaa357eec685c5d55cabf3fde5909ff30ec5822
abdul <abdul@easyfundraising.org.uk>
2016-06-20 17:21:36 +0100

    Interface enforcing these methods now

--- src/Context/Interfaces/SQLContextInterface.php
+++ src/Context/Interfaces/SQLContextInterface.php
@@ -23,4 +23,44 @@ interface SQLContextInterface
     public function iSaveTheIdAs($key);
 
     public function iAmInDebugMode();
+
+    /**
+     * User friendly version of iDontHaveAWhere.
+     *
+     * @param string $table The table to delete from.
+     * @param string $where The where clause.
+     *
+     * @return string
+     */
+    public function delete($table, $where);
+
+    /**
+     * User friendly version of iHaveAWith.
+     *
+     * @param $table The table to insert into.
+     * @param $values Values to insert.
+     *
+     * @return string
+     */
+    public function insert($table, $values);
+
+    /**
+     * User friendly version of iHaveAnExistingWithWhere.
+     *
+     * @param string $table The table to delete from.
+     * @param string $where The where clause.
+     *
+     * @return string
+     */
+    public function update($table, $update, $where);
+
+    /**
+     * User friendly version of iHaveAnExistingWhere.
+     *
+     * @param string $table The table to delete from.
+     * @param string $where The where clause.
+     *
+     * @return string
+     */
+    public function select($table, $where);
 }
