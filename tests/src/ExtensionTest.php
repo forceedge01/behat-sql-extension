0d6193098f58144ce798368eaaa74bdb7c7b1353
darrenpotter <darren@easyfundraising.org.uk>
2018-12-14 08:26:45 +0000

    Fixing the mock object definitions in ExtensionTest::testConfigure() so all methods mocked exist without having to define them first. This is important as defining the methods you're then going to test means that you're testing your test, not the code.

--- tests/src/ExtensionTest.php
+++ tests/src/ExtensionTest.php
@@ -6,74 +6,85 @@ use Genesis\SQLExtension\Extension;
 use PHPUnit_Framework_TestCase;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Symfony\Component\Config\Definition\Builder\ScalarNodeDefinition;
 use Symfony\Component\Config\Definition\ScalarNode;
 
 class ExtensionTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Extension
+     */
     private $extension;
 
-    public function __construct()
+    protected function setUp()
     {
         $this->extension = new Extension();
     }
 
-    public function testLoad()
-    {
-        // $containerBuilderMock = $this->getMockBuilder(ContainerBuilder::class)
-        //     ->disableOriginalConstructor()
-        //     ->getMock();
-
-        // $config = [];
-
-        // $this->extension->load($config, $containerBuilderMock);
-    }
-
     public function testConfigure()
     {
-        $arrayNodeDefinitionMock = $this->getMockBuilder(ArrayNodeDefinition::class)
+        /** @var ArrayNodeDefinition|\PHPUnit_Framework_MockObject_MockObject $arrayNodeDefinitionMock1 */
+        $arrayNodeDefinitionMock1 = $this->getMockBuilder(ArrayNodeDefinition::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-        $scalarNodeMock = $this->getMockBuilder(ScalarNode::class)
+        $arrayNodeDefinitionMock2 = $this->getMockBuilder(ArrayNodeDefinition::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-        $nodeBuilderMock = $this->getMockBuilder(NodeBuilder::class)
-            ->setMethods(['defaultValue', 'end'])
+        $nodeBuilderMock1 = $this->getMockBuilder(NodeBuilder::class)
             ->getMock();
 
-        $nodeBuilderMock->expects($this->any())
+        $nodeBuilderMock2 = $this->getMockBuilder(NodeBuilder::class)
+            ->getMock();
+
+        $scalarNodeDefinitionMock = $this->getMockBuilder(ScalarNodeDefinition::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $arrayNodeDefinitionMock1->expects($this->any())
+            ->method('children')
+            ->will($this->returnValue($nodeBuilderMock1));
+
+        $nodeBuilderMock1->expects($this->any())
+            ->method('arrayNode')
+            ->will($this->returnValue($arrayNodeDefinitionMock2));
+
+        $arrayNodeDefinitionMock2->expects($this->any())
+            ->method('children')
+            ->will($this->returnValue($nodeBuilderMock2));
+
+        $nodeBuilderMock2->expects($this->any())
             ->method('scalarNode')
-            ->will($this->returnValue($scalarNodeMock));
+            ->will($this->returnValue($scalarNodeDefinitionMock));
 
-        $nodeBuilderMock->expects($this->any())
-            ->method('end')
-            ->will($this->returnSelf());
-
-        $nodeBuilderMock->expects($this->any())
+        $scalarNodeDefinitionMock->expects($this->any())
             ->method('defaultValue')
             ->with($this->logicalOr($this->isType('string'), null))
             ->will($this->returnSelf());
 
-        $nodeBuilderMock->expects($this->any())
+        $scalarNodeDefinitionMock->expects($this->any())
+            ->method('end')
+            ->will($this->returnValue($nodeBuilderMock2));
+
+        $nodeBuilderMock2->expects($this->any())
+            ->method('end')
+            ->will($this->returnValue($arrayNodeDefinitionMock2));
+
+        $arrayNodeDefinitionMock2->expects($this->any())
+            ->method('end')
+            ->will($this->returnValue($nodeBuilderMock1));
+
+        $arrayNodeDefinitionMock2->expects($this->any())
             ->method('ignoreExtraKeys')
             ->with(false)
             ->will($this->returnSelf());
 
-        $arrayNodeDefinitionMock->expects($this->any())
-            ->method('children')
-            ->will($this->returnValue($nodeBuilderMock));
+        $nodeBuilderMock1->expects($this->any())
+            ->method('end')
+            ->will($this->returnValue($arrayNodeDefinitionMock1));
 
-        $nodeBuilderMock->expects($this->any())
-            ->method('arrayNode')
-            ->will($this->returnValue($arrayNodeDefinitionMock));
-
-        $scalarNodeMock->expects($this->any())
-            ->method('defaultValue')
-            ->with($this->logicalOr($this->isType('string'), null))
-            ->will($this->returnValue($nodeBuilderMock));
-
-        $this->extension->configure($arrayNodeDefinitionMock);
+        $this->extension->configure($arrayNodeDefinitionMock1);
     }
 
     /**
